669f30176f77697574ee619cb849c2a9b684bf03
rus <kipnis.rus@gmail.com>
2022-01-05 23:36:40 -0500

    adding complete js functions, 3 are missing

---
+++ index.js
@@ -0,0 +1,177 @@
+let numbers = [10,32,341,3,1,6];
+//1---------1-------------1-----------------1
+  function justEl(element){
+    return element;
+  }
+  // forEach() function
+  function myEach(arr,cb)
+  {
+    //const forEach g= [];
+      for(let i =0; i < arr.length; i++)
+      {
+       console.log(cb(arr[i]));
+          //forEach.push(arr[i]);
+        //return arr[i];
+      }
+   // return forEach;
+  }
+  //console.log(myEach(numbers));
+  myEach(numbers,justEl);
+//array [10,32,341,3,1,6]
+//2---------2-------------2-----------------2
+  function product(element)
+  {
+    return element*2;
+  }
+  // Map() function
+  function myMap(num, cb)
+  {
+    const newArray = []
+    for(let a of num)
+      {
+        //newArray.push(a);
+       newArray.push(cb(a));
+      }
+    return newArray;
+  }
+  console.log(myMap(numbers,product));
+//array [10,32,341,3,1,6]
+//3---------3-------------3-----------------3
+  function even(element)
+    {
+      return element%2;
+    }
+  // filter() function
+  function myFilter(arr,callback)
+  {
+    const newArr = [];
+    for(let i=0; i<arr.length; i++)
+      {
+        if(callback(arr[i])==0)
+          {
+            newArr.push(arr[i])
+          } 
+      }
+    return newArr;
+  }
+  console.log(myFilter(numbers,even));
+//array [10,32,341,3,1,6]
+//4---------4-------------4-----------------4
+  function odd (element) 
+  {
+    return element%2;
+  }
+  // some() function
+  function mySome(arr,callback)
+  {
+    for(let a of arr)
+      {
+        if(callback(a) == 0)
+          {
+            return true;
+          }
+        else{return false;}
+      }
+  }
+  console.log(mySome(numbers,odd));
+//array [10,32,341,3,1,6]
+//5---------5-------------5-----------------5
+  function value(element)
+  {
+     return element>100;
+  }
+  // every() function
+  function myEvery(arr,callback)
+  {
+  //   for(let a of arr)
+    for(let i =0; i<arr.length; i++)
+      { 
+          if(callback[i])
+          {
+             return true;
+          }
+          else{return false;}
+      }
+  }
+  console.log(myEvery(numbers,value));
+//array [10,32,341,3,1,6]
+//6---------6-------------6-----------------6
+  function callBack(element)
+  {
+    return element;
+  }
+  function myReduce(arr, callback)
+  {
+   let sum = 0;
+    for(let i = 0; i < arr.length; i++) 
+      {
+        sum+=callback(arr[i]);
+      }
+    return sum;
+  }
+  console.log(myReduce(numbers,callBack));
+//array [10,32,341,3,1,6]
+//7---------7-------------7-----------------7
+// includes() function
+  function myIncludes(arr,callback,target)
+  {
+    for(let a of arr)
+      {
+        if(callback(a) == arr[a])
+          {
+            return true;
+          }
+      }
+  }
+  console.log(myIncludes(numbers,callBack,341));
+//8---------8-------------8-----------------8
+// indexOf() function
+  function myIndexOf(arr, target,callback)
+  {
+    for(let i = 0; i < arr.length; i++) 
+      {
+        //if(callback(arr[i]) === arr[target])
+        if(callback(arr[i])===target)
+          {
+            return i;
+          }
+      }
+    return -1;
+  }
+  console.log(myIndexOf(numbers,6,callBack));
+//array [10,32,341,3,1,6]
+//9---------9-------------9-----------------9
+// push() function
+  function myPush(arr, elementToAdd, callback)
+  {
+    let newArray = [];
+   // newArray = arr.concat(elementToAdd);
+    newArray = [...arr,elementToAdd];
+    return callback(newArray);
+  }
+  console.log(myPush(numbers,700,callBack));
+//10--------10------------10----------------
+// lastIndexOf() function
+//incomplete.........
+  function myLastIndexOf(arr, target, callback)
+  {
+    for(let i = 0; i < arr.length; i++) 
+      {
+        if(arr[i]===target)
+        {
+          return callback(arr[i]);
+        }
+      }
+        return -1;
+  }
+console.log(myLastIndexOf(numbers,6,callBack));
+//array [10,32,341,3,1,6]
+//11--------11------------11----------------
+function grabKeys(){}
+
+
+
+
+
+//12--------12------------12----------------
+function grabValues(){}
